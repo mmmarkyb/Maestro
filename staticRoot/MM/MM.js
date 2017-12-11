adb9f406cafbfcfd41619da953892b4de1505a19
Karla Geron <krlgeron@yahoo.co.uk>
2017-12-11 02:18:56 +0000

    RIP

--- staticRoot/MM/MM.js
+++ staticRoot/MM/MM.js
@@ -1,5 +1,5 @@
 // Another drag and drop mechanism
-var selectedElement = 0;
+/*var selectedElement = 0;
 var currentX = 0;
 var currentY = 0;
 var currentMatrix = 0;
@@ -38,4 +38,35 @@ function deselectElement(evt) {
 }
 
 selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)");
-selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");
+selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");*/
+
+// This better work
+
+var click=false; // flag to indicate when shape has been clicked
+    var clickX, clickY; // stores cursor location upon first click
+    var moveX=0, moveY=0; // keeps track of overall transformation
+    var lastMoveX=0, lastMoveY=0; // stores previous transformation (move)
+    function mouseDown(evt){
+        evt.preventDefault(); // Needed for Firefox to allow dragging correctly
+        click=true;
+        clickX = evt.clientX;
+        clickY = evt.clientY;
+        evt.target.setAttribute("fill","green");
+    }
+
+    function move(evt){
+        evt.preventDefault();
+        if(click){
+            moveX = lastMoveX + ( evt.clientX – clickX );
+            moveY = lastMoveY + ( evt.clientY – clickY );
+
+            evt.target.setAttribute("transform", "translate(" + moveX + "," + moveY + ")");
+        }
+    }
+
+    function endMove(evt){
+        click=false;
+        lastMoveX = moveX;
+        lastMoveY = moveY;
+        evt.target.setAttribute("fill","gray");
+    }
